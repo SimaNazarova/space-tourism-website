4c2a42722909208157fc834fd966a0669244c4ac
Serafima Nazarova <sima.nazarova@gmail.com>
2025-02-10 11:47:12 +0300

    доработка home

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,16 +1,16 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 
 import styles from "./Home.module.scss";
 import exploreImg from "../../../images/explore.png";
 import gsap from "gsap";
 function Home() {
-  const imgRef = useRef<any>();
+  const imgRef = useRef<HTMLImageElement>(null);
 
-  function anma(status: string) {
+  function showCircleAnimation(status: string) {
     const tl = gsap.fromTo(
       imgRef.current,
-      { scale: 1, duration: 0.12 },
-      { scale: 1.2, duration: 0.12 }
+      { scale: 1, duration: 0.5 },
+      { scale: 1.2, duration: 0.5 }
     );
     if (status === "show") {
       tl.play();
@@ -18,7 +18,7 @@ function Home() {
       tl.reverse(0);
     }
   }
-  console.log(imgRef.current);
+
   return (
     <div className={styles.container}>
       <div className={styles.textContainer}>
@@ -27,17 +27,18 @@ function Home() {
           <span> SPACE</span>
         </h1>
         <p className={styles.text}>
-          Let’s face it; if you want to go to space, you might as well <br />{" "}
-          genuinely go to outer space and not hover kind of on the <br /> edge
-          of it. Well sit back, and relax because we’ll give you a <br />
-          truly out of this world experience!
+          Let&rsquo;s face&nbsp;it; if&nbsp;you want
+          to&nbsp;go&nbsp;to&nbsp;space, you might as&nbsp;well genuinely
+          go&nbsp;to&nbsp;outer space and not hover kind of&nbsp;on&nbsp;the
+          edge of&nbsp;it. Well sit back, and relax because we&rsquo;ll give you
+          a&nbsp;truly out of&nbsp;this world experience!
         </p>
       </div>
       <div className={styles.imgContainer}>
         <div className={styles.imgOpacity} ref={imgRef}></div>
         <img
-          onMouseEnter={() => anma("show")}
-          onMouseLeave={() => anma("hide")}
+          onMouseEnter={() => showCircleAnimation("show")}
+          onMouseLeave={() => showCircleAnimation("hide")}
           className={styles.img}
           src={exploreImg}
           alt="explore"
